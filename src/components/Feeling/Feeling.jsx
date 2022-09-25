92084e8cbf3951778bb58055fa2bd2984f270e4c
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-09-25 17:17:35 -0500

    completed a reducer for feeling page

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,12 +1,30 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+
+
+
 
 const Feeling = () => {
+    const feeling = useSelector(store => store.feeling); // getter
+    const dispatch = useDispatch();
 
+    const handleChange = (event) => {
+        // Pass data to reducer
+        dispatch({ type: 'SET_FEELING', payload: event.target.value})
+    }
     return (
         <>
             <h1>How are you feeling today?</h1>
             <h3>Feeling?</h3>
-            <input></input>
-            <button className="button">Next</button>
+            <div>
+                {/* getter and setter */}
+                <input value={feeling} onChange={handleChange}
+                       className="input" type="number" min="1" max="5"
+                >
+                </input>
+                <button className="button">Next</button>
+            </div>
         </>
 
     )
--- src/index.js
+++ src/index.js
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feeling = (state = 1, action) => {
+    if (action.type === 'SET_FEELING') {
+        // dispatch will have type of 'SET_FEELING'
+        // and payload with the value to set
+        return action.payload
+    }
+    return state;
+}
+
+// Redux store to keep track of all reducers.
+const storeInstance = createStore(
+    combineReducers(
+        {
+           feeling, 
+        }
+    ),
+    applyMiddleware(logger)
+)
+
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
+
+
+
 registerServiceWorker();
