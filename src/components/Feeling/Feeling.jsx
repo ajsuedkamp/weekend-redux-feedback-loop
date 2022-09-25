e8b31901c28f8d3bc59ab01125caa838315e89f1
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-09-25 10:26:52 -0500

    setup component structure for the 4 input forms

---
+++ src/components/Feeling/Feeling.jsx
@@ -0,0 +1,13 @@
+
+const Feeling = () => {
+
+    return (
+        <>
+            <p>How are you feeling today?</p>
+            <h3>Feeling?</h3>
+        </>
+
+    )
+}
+
+export default Feeling;
